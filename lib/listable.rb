a7affd82ea4e0ff48c6b86daa484357b898a262e
Ivan Mironov <ivan.mironov@gmail.com>
2016-04-23 15:25:06 +0300

    moved format_date to Listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,11 @@ module Listable
   def format_description(description)
     "#{description}".ljust(25)
   end
+
+  def format_date(dates, empty_message)
+    dates.compact!
+    return empty_message if dates.empty?
+    dates.map! { |date| date.strftime("%D") }
+    dates.join(' -- ')
+  end
 end
